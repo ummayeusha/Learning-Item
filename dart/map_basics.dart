0f21a06622ade2b544c9f8897f04345fbd46f859
ummayeusha <ummayeusha123@gmail.com>
2025-05-18 21:39:01 +0600

    map basics added

---
+++ dart/map_basics.dart
@@ -0,0 +1,29 @@
+main() {
+  Map<String, String> person = {'name': 'eueu', 'age': '22'};
+
+  print(person);
+  print("what's name?");
+  print(person['name']);
+  print("what's age?");
+  print('my age: ${person['age']}');
+
+  person.remove('age');
+  print('my age: ${person['age']}');
+
+  print(person.containsKey('age'));
+  print(person.containsKey('name'));
+
+  print(person.containsValue('22'));
+  print(person.containsValue('eueu'));
+
+  Map<String, String> extra_info = {'sub': 'cse', 'cg': '2.2'};
+  person.addAll(extra_info);
+  print(person);
+  print(person.length);
+
+  var keyList = person.keys.toList();
+  print(keyList);
+
+  var valueList = person.values.toList();
+  print(valueList);
+}
